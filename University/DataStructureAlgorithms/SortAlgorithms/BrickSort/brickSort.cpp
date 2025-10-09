926389256690b44ca705ef1a338f7c653d381ffa
Antkurom <antonskurockins@gmail.com>
2025-10-09 15:47:52 +0300

    Starting new course, Algorithms and Data Structures

---
+++ University/DataStructureAlgorithms/SortAlgorithms/BrickSort/brickSort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int array[] = {2, 5, 3, 0, 2, 6, 1};
+	int size = 7;
+	int changes = 1;
+	while (changes != 0){
+		// Odd phase
+		for(int i = 1; i<size; i+=2){
+			
+		}
+	}
+}
