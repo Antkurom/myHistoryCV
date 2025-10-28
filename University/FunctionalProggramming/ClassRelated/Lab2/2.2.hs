1b9bcb3cc1a1828660fbdf17e1a202ea12603660
Antkurom <antonanet@yandex.ru>
2025-10-28 22:07:58 +0200

    Last Class Work for Functional Programming

--- University/FunctionalProggramming/ClassRelated/Lab2/2.2.hs
+++ University/FunctionalProggramming/ClassRelated/Lab2/2.2.hs
@@ -1,6 +1,10 @@
-sortArraysByLength :: [[a]] -> [Int] -> [[a]]
-sortArraysByLength x:xs y:ys = if 
+runLengthEncoding :: [Int] -> [(Int, Int)]
+runLengthEncoding x:xs = 
 
+countNumbers :: [Int] -> Int -> Int -> (Int, [Int])
+countNumbers x:xs y acc = if x==y then 
+    countNumbers xs y acc+1
+    else (acc, x:xs)
 
 main :: IO()
 main = do 
